4a2ace2574f05878d3c80e23ad0cd1c6ad150337
Evgeniy Dubovskoy <edubovskoy@gmail.com>
2025-05-13 01:10:10 -0700

    remove lspconfig fix as the mason version is freezed now in LazyVim

--- lua/plugins/lspconfig-fix.lua
+++
@@ -1,5 +0,0 @@
-# TODO: Remove this version pinning when fix is released: https://github.com/LazyVim/LazyVim/pull/6041
-return {
-  { "mason-org/mason.nvim", version = "1.11.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
-}
