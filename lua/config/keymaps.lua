58dc017060ca1588787c8267d161651d39f6f381
Yevhen Dubovskoi <ydubovskoi@google.com>
2025-05-19 23:19:54 +0000

    add OSC 52 clipboard; change vimwiki bind to toggle list items (fix for MacOS)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Vimwiki bind to toggle TODO list items
+vim.keymap.set("n", "<A-x>", "<cmd>VimwikiToggleListItem<CR>", { desc = "(Vimwiki) Toggle list item" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.o.conceallevel = 0
+
+-- activate OSC 52 clipboard to be able to copy via SSH (more to read here: https://neovim.io/doc/user/provider.html#clipboard-osc52)
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
