7651f28a2cb2f6a668d7e532d8dcec68a38d8d16
Yevhen Dubovskoi <edubovskoy@gmail.com>
2025-08-30 08:30:13 +0000

    implement temporary fix for catppuccin theme; remove osc52 slipboard setup

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,11 +4,15 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    opts = {
-      no_italic = true,
-    },
+		opts = function(_, opts)
+			local module = require("catppuccin.groups.integrations.bufferline")
+			if module then
+				module.get = module.get_theme
+			end
+      opts.no_italic = true
+			return opts
+		end,
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
