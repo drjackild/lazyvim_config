e14c727f60bf0f6db9abce8cde6329c1e3baae98
Yevhen Dubovskoi <edubovskoy@gmail.com>
2025-12-08 00:35:26 -0800

    remove temporary catppuccin fix as no longer needed

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,14 +4,6 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-		opts = function(_, opts)
-			local module = require("catppuccin.groups.integrations.bufferline")
-			if module then
-				module.get = module.get_theme
-			end
-      opts.no_italic = true
-			return opts
-		end,
   },
   {
     "LazyVim/LazyVim",
